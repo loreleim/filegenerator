bbdcc24ed8b112726db67e3d27ce00c2c79d726d
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-26 12:02:25 -0800

    adding in a basic database for later

---
+++ src/database/index.js
@@ -0,0 +1,16 @@
+class Database {
+  constructor() {
+    this.multidimensionalArray = {
+      About: [
+        {
+          name: "CDL Mockups",
+        }
+      ]
+    };
+
+    this.html = "<h1>Hey</h1>"
+  }
+}
+
+const store = new Database();
+export default store;

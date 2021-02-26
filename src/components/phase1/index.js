01f47905a0d1930cca4ad1d04d5ed7895e542747
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-26 10:48:54 -0800

    updating test with app name

--- src/components/phase1/index.js
+++ src/components/phase1/index.js
@@ -3,7 +3,7 @@ import style from "./index.module.scss";
 export default function Phase1() {
   return (
     <div className={style.phaseParent}>
-      <h1>Phase 1</h1>
+      <h1>File Generator</h1>
     </div>
   );
 }

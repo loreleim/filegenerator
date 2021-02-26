f7e7c9d63f4c55bf9105854c2acbedc7f3cd7217
Lorelei M <lorelei.miyamura@gmail.com>
2021-02-26 15:42:44 -0800

    phase 2 complete!
    next phase is trying to zip things up

--- src/components/phase2/index.js
+++ src/components/phase2/index.js
@@ -1,21 +1,43 @@
 import style from "./index.module.scss";
-import store from "../../database";
+import React, { useState } from "react";
 
 export default function Phase2() {
 
+  //this is the themeName listener, that changes on input keyup
+  const [themeName, setThemeName] = useState("default");
+
+  var indexphp =
+`
+<?php get_header(); ?>
+	
+<main class="${themeName}-content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php the_content() ?>
+    <?php endwhile; endif; ?>
+</main>
+  
+<?php get_footer(); ?>
+`
+
   function php() {
     const htmlElement = document.createElement("a");
-    const file = new Blob([store.indexphp], {type : 'application/php'});
+    const file = new Blob([indexphp], {type : 'application/php'});
     htmlElement.href = URL.createObjectURL(file);
     htmlElement.download = "index.php";
     document.body.appendChild(htmlElement);
     htmlElement.click();
   }
 
+  //this listens to the letters that are typed into input
+  //happens on keyup with the onChange hook
+  function themeNameListener(e) {
+    setThemeName(e.target.value);
+  }
+
   return (
     <div className={style.phaseParent}>
       <h1>File Generator Phase 2</h1>
-      <input type="text"></input>
+      <input type="text" onChange={themeNameListener}></input>
       <button onClick={() => php()}>PHP</button>  
     </div>
   );

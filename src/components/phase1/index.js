d0bf0c779102d0858e36d0e2de7de4384bf09152
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-02-26 11:15:04 -0800

    working .txt file gen

--- src/components/phase1/index.js
+++ src/components/phase1/index.js
@@ -1,6 +1,18 @@
 import style from "./index.module.scss";
 
 export default function Phase1() {
+
+  function downloadTxtFile() {
+    const htmlElement = document.createElement("a");
+    const file = new Blob(["Hey Testing!"], {type: 'text/plain;charset=utf-8'});
+    
+    //var aFileParts = ['<a href="https://google.com"><b id="b">hey!</b></a>'];
+    //const file = new Blob(aFileParts, {type : 'text/html'});
+    htmlElement.href = URL.createObjectURL(file);
+    htmlElement.download = "myFile.txt";
+    document.body.appendChild(htmlElement);
+    htmlElement.click();
+  }
   return (
     <div className={style.phaseParent}>
       <h1>File Generator</h1>
